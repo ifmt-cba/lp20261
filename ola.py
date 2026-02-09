4d384e0b913fbaa92e0746d00790a2dc3b6398de
Preti, João Paulo <108187064+preti-joao@users.noreply.github.com>
2026-02-09 15:16:41 +0000

    criado comando diga_ola

--- ola.py
+++ ola.py
@@ -1,1 +1,7 @@
-print('Olá Mundo!!!')
+def diga_ola():
+    nome = input('Digite seu nome: ')
+    print(f'Olá {nome}! Seja bem vindo(a).')
+    idade = int(input('Por favor me diga sua idade: '))
+    print(f'Perfeito {nome}. Você tem {idade} anos.')
+
+#diga_ola()
