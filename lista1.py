a7b75488ed79cea7f1068942e3d1b48f3dd3b046
Preti, João Paulo <108187064+preti-joao@users.noreply.github.com>
2026-02-11 15:20:36 +0000

    q1-q5

--- lista1.py
+++ lista1.py
@@ -3,14 +3,30 @@ Exercícios sobre os comandos básicos em Python
 '''
 
 #1. Faça um programa que imprima o seu nome.
+def q1():
+    print('João Paulo')
 
 #2. Faça um programa que imprima o produto dos valores 30 e 27.
+def q2():
+    print(30*27)
 
 #3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
+def q3():
+    media = (5+8+12)/3
+    print(f'(5+8+12)/3 = {media}')
 
 #4. Faça um programa que leia e imprima um número inteiro.
+def q4():
+    numero = int(input('Digite um número inteiro: '))
+    print(f'Você digitou: {numero}')
 
 #5. Faça um programa que leia dois números reais e os imprima.
+def q5():
+    num1 = float(input('Digite um número real: '))
+    num2 = float(input('Digite outro número real: '))
+    print(f'Os números digitados foram: {num1:.1f} e {num2:.2f}')
+
+q5()
 
 #6. Faça um programa que leia um número inteiro e imprima o seu
 #   antecessor e o seu sucessor.
