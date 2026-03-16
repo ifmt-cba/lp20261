a2dd3f62e5e1254bab66c8ae63155dcdaa3e2437
Preti, João Paulo <jppreti@gmail.com>
2026-03-16 10:38:31 -0400

    lista2: q10

--- lista2.py
+++ lista2.py
@@ -131,6 +131,18 @@ def q9():
 
 #10. Faça um programa que leia três números inteiros e imprima os três em ordem
 #crescente.
+def q10():
+    a = inputint('Digite um número inteiro: ')
+    b = inputint('Digite um número inteiro: ')
+    c = inputint('Digite um número inteiro: ')
+    resultado=""
+    resultado += f"{a} {b} {c}" if a<b<c else ""
+    resultado += f"{a} {c} {b}" if a<c<b else ""
+    resultado += f"{b} {a} {c}" if b<a<c else ""
+    resultado += f"{b} {c} {a}" if b<c<a else ""
+    resultado += f"{c} {a} {b}" if c<a<b else ""
+    resultado += f"{c} {b} {a}" if c<b<a else ""
+    print(resultado)
 
 #11. Faça um programa que leia 3 números e imprima o maior deles.
 def q11():
