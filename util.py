00b701c9c5de24f381d21140540af5848e38dd1d
Preti, João Paulo <jppreti@gmail.com>
2026-03-23 10:27:10 -0400

    util com repeticao

--- util.py
+++ util.py
@@ -19,15 +19,17 @@ def inputint(msg="Digite um valor inteiro: ",min=None,max=None):
             print(e)
 
 def inputfloat(msg="Digite um número real: ",min=None,max=None):
-    try:
-        valor = float(input(msg))
-        if min!=None and valor < min:
-            raise Exception(f'ERRO: valor é menor do que o mínimo permitido de {min}')
-        if max!=None and valor > max:
-            raise Exception(f'ERRO: valor é maior do que o máximo permitido de {max}')
-        return valor
-    except ValueError:
-        print ('ERRO: Valor informado não é um número real!')
-    except Exception as e:
-        print(e)
-    return -1
+    erro = True
+    while erro == True:
+        try:
+            valor = float(input(msg))
+            if min!=None and valor < min:
+                raise Exception(f'ERRO: valor é menor do que o mínimo permitido de {min}')
+            if max!=None and valor > max:
+                raise Exception(f'ERRO: valor é maior do que o máximo permitido de {max}')
+            erro = False
+            return valor
+        except ValueError:
+            print ('ERRO: Valor informado não é um número real!')
+        except Exception as e:
+            print(e)
