f0d797aebe835c7d2a3e443932950f247945a575
Preti, João Paulo <jppreti@gmail.com>
2026-04-15 11:14:35 -0400

    Lista3: q12 com gráfico de linha

--- lista3.py
+++ lista3.py
@@ -4,6 +4,7 @@ Lista de Exercícios referentes a estruturas de iteração (repetição)
 from util import inputint, inputfloat, gerar_palavra
 import random
 from typing import Final
+import matplotlib.pyplot as plt
 
 VERMELHO: Final = '\033[31m'
 VERDE: Final = '\033[32m'
@@ -138,10 +139,18 @@ def q11() -> None:
 #ano, fazer um programa que calcule e imprima o tempo necessário para que a
 #população do país A ultrapasse a população do país B.
 def q12() -> None:
+    xA = []
+    yA = []
+    xB = []
+    yB = []
     paisA: int = 5_000_000
     paisB: int = 7_000_000
     ano: int = 0
     while paisA < paisB:
+        xA.append(ano)
+        yA.append(paisA)
+        xB.append(ano)
+        yB.append(paisB)
         ano+=1
         paisA = paisA*1.03
         paisB = paisB*1.02
@@ -149,18 +158,49 @@ def q12() -> None:
     print(f'População do país A = {int(paisA)}')
     print(f'População do país B = {int(paisB)}')
 
+    # Criar o gráfico
+    plt.ticklabel_format(style='plain', axis='y') # 'plain' mostra o número cheio
+    plt.plot(xA, yA, marker='.', linestyle='-', color='b', label='País A')
+    plt.plot(xB, yB, marker='.', linestyle='-', color='r', label='País B')
+
+    # Adicionar títulos e rótulos
+    plt.title('Crescimento Populacional dos Países A e B')
+    plt.xlabel('Ano')
+    plt.ylabel('População')
+    plt.legend(loc='best')
+
+    # Exibir
+    plt.show()
+
 #12.1 Faça uma simulação de investimento para que se descubra quantos anos serão necessários
 # para que se alcance a marca de 1 milhão de reais a partir de um saldo inicial, 
 # um aporte mensal regular e uma taxa de retorno mensal constante.
 def q121() -> None:
+    x = []
+    y = []
+    meta: int = inputint('Meta de investimento: R$ ',min=1000)
     saldo: float = inputfloat('Saldo inicial: R$ ', min=0)
     aporte: float = inputfloat('Aporte mensal: R$ ', min=0)
     taxa: float = inputfloat('Taxa de retorno mensal (%): ', min=0.1)
     mes: int = 0
-    while saldo < 1_000_000:
+    while saldo < meta:
         mes += 1
         saldo = saldo + aporte + saldo*taxa/100
+        x.append(mes)
+        y.append(saldo)
     print(f'Tempo do investimento: {int(mes/12)} anos e {mes%12} meses.')
+
+    # Criar o gráfico
+    plt.ticklabel_format(style='plain', axis='y') # 'plain' mostra o número cheio
+    plt.plot(x, y, linestyle='-', color='b')
+
+    # Adicionar títulos e rótulos
+    plt.title('Histórico do Saldo do Investimento')
+    plt.xlabel('Mês')
+    plt.ylabel('Saldo')
+
+    # Exibir
+    plt.show()
 
 #13. Uma empresa de fornecimento de energia elétrica faz a leitura mensal dos medidores
 #de consumo. Para cada consumidor, são digitados os seguintes dados:
