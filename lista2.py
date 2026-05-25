e16b9767bc599d2d0bf498ef1f2647d3bf115a53
Preti, João Paulo <jppreti@gmail.com>
2026-05-25 10:45:00 -0400

    lista2: q12 exemplo com tkinter

--- lista2.py
+++ lista2.py
@@ -4,6 +4,7 @@ Exercícios sobre os comandos de condição em python
 import random
 from datetime import datetime
 from util import inputfloat, inputint
+from tkinter import Tk, Label, Entry, Button, messagebox
 
 def exemploSe():
     idade = 18
@@ -159,6 +160,34 @@ def q11():
 #• Se é maior de idade
 #• Se é menor de idade
 #• Se é maior de 65 anos
+def q12():
+    def show_idade():
+        idade = int(txt_idade.get())
+        msg=''
+        if idade <18:
+            msg = 'Menor de Idade'
+        elif idade >= 65:
+            msg = 'Melhor idade'
+        else:
+            msg = 'Maior idade'
+        messagebox.showinfo(
+            title='Situação da Idade:',
+            message=f'{msg}'
+        )
+        txt_idade.delete(0,len(txt_idade.get()))
+        
+    window = Tk()
+    window.title('Questão 12')
+    window.config(padx=10, pady=10)
+    lbl_idade = Label(text='Idade:')
+    lbl_idade.grid(row=0, column=0)
+    global txt_idade
+    txt_idade = Entry(width=4)
+    txt_idade.grid(row=0,column=1)
+    txt_idade.focus()
+    btn_ok = Button(text='OK', width=5, command=show_idade)
+    btn_ok.grid(row=1, column=0, columnspan=2)
+    window.mainloop()
 
 #13. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota
 #da prova 2 de um aluno. O programa deve imprimir o nome, a nota da prova 1,
